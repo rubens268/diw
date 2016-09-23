7f98ef05636a2649b37da45aa63d1bdd667f02b9
ciclos <ciclos>
2016-09-23 18:04:31 +0200

    22 - review (23-09-16)

--- U2/EJ12/ej12.js
+++ U2/EJ12/ej12.js
@@ -1,5 +1,38 @@
-var num = prompt("Que tabla de multiplicar quieres ver hoy?");
+var fecha = new Date();
+var dia = fecha.getDay();
 
-for (var i = 0; i <= 10; i++) {
-	document.write("<p>" + num + " x " + i + " = " + (num*i) + "</p>");
+var dia;
+var restantes;
+switch(dia)
+{
+	case 0:
+		dia = "domingo";
+		restantes = "0";
+	break;
+	case 1:
+		dia = "lunes";
+		restantes = "6";
+	break;
+	case 2:
+		dia = "martes";
+		restantes = "5";
+	break;
+	case 3:
+		dia = "miercoles";
+		restantes = "4";
+	break;
+	case 4:
+		dia = "jueves";
+		restantes = "3";
+	break;
+	case 5:
+		dia = "viernes";
+		restantes = "2";
+	break;
+	case 6:
+		dia = "sabado";
+		restantes = "1";
+	break;
 }
+
+document.write("Hoy es " + dia + ", quedan " + restantes + " días para el domingo.");
